93299ae1accf51f7286c2575de08c94a5b4c905c
rezak400 <k400ezza@gmail.com>
2021-01-12 17:10:46 +0700

    tes push

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
         <Layout title="Home">
           <div className=" p-10 py-24 container">
             <h1 className="text-5xl">Hello, Perkenalkan saya</h1>
-            <p className="text-3xl">Reza Dwiputra</p>
+            <p className="text-3xl">Muhammad Reza Dwiputra</p>
           </div>
         </Layout>    
   )
