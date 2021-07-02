39af584cedebab5ee23932910ad7e7ad65a22f8b
rezak400 <k400ezza@gmail.com>
2021-07-02 11:24:04 +0700

    add Label, fix style

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,17 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Button(props) {
-    return (
-        <div>
-            <Link href={props.target}>
-                <a target="_blank" className="bg-gray-200 py-2 px-3 rounded-full text-gray-800 font-normal flex hover:bg-white items-center font-primary text-xs mt-6 md:mt0 shadow-inner" >
-                    <img src={props.image} alt="" className="w-6" />
-                    <p className="ml-2">{props.title}</p>
-                </a>
-            </Link>
-        </div>
-    )
+  return (
+    <div>
+      <Link href={props.target}>
+        <a
+          target="_blank"
+          className="bg-gray-200 py-2 pl-3 pr-6 rounded-full text-gray-800 font-normal flex hover:bg-white items-center font-primary text-xs mt-6 md:mt0 shadow-inner"
+        >
+          <img src={props.image} alt="" className="w-6" />
+          <p className="ml-2">{props.title}</p>
+        </a>
+      </Link>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,47 @@ import Layout from "../components/Layout";
 import Link from "next/link";
 import Button from "../components/Button";
 
-
 export default function Home() {
   return (
     <Layout title="Home">
       <div className="md:h-screen bg-primary flex items-center  overflow-hidden">
         <div className="container text-white">
-
           {/* HEADER */}
           <div className="md:flex flex-row-reverse md:pt-12">
             {/* image */}
-            <div data-wow-duration="2s" data-wow-delay="0.5s" className="md:w-1/2 wow fadeInUp">
-              <img src="profile.png" alt="" className="mx-auto w-40 md:w-52 mt-16 md:mt-0" />
+            <div
+              data-wow-duration="2s"
+              data-wow-delay="0.5s"
+              className="md:w-1/2 wow fadeInUp"
+            >
+              <img
+                src="profile.png"
+                alt=""
+                className="mx-auto w-40 md:w-52 mt-16 md:mt-0"
+              />
             </div>
 
             {/* Headline */}
-            <div data-wow-duration="2s" className="wow fadeInLeft md:w-1/2 md:pl-16 mt-8 md:mt-0 text-center md:text-left md:py-6 ">
-              <h1 className="md:text-5xl text-3xl font-primary font-medium">Reza Dwiputra</h1>
+            <div
+              data-wow-duration="2s"
+              className="wow fadeInLeft md:w-1/2 md:pl-16 mt-8 md:mt-0 text-center md:text-left md:py-6 "
+            >
+              <h1 className="md:text-5xl text-3xl font-primary font-medium">
+                Reza Dwiputra
+              </h1>
               <ul data-wow-delay="2s" className="md:list-disc sm:ml-5 ">
-                <li className=" text-base md:text-xl font-secondary mt-5 font-thin">Web Developer</li>
-                <li className="text-base md:text-xl font-secondary mt-1 font-thin">Writer</li>
-                <li className="text-base md:text-xl font-secondary mt-1 font-thin">Curious Person</li>
+                <li className=" text-base md:text-xl font-secondary mt-5 font-thin">
+                  Web Developer
+                </li>
+                <li className="text-base md:text-xl font-secondary mt-1 font-thin">
+                  Writer
+                </li>
+                <li className="text-base md:text-xl font-secondary mt-1 font-thin">
+                  Curious Person
+                </li>
+                <li className="text-base md:text-xl font-secondary mt-1 font-thin">
+                  Lazy Perfectionist
+                </li>
               </ul>
             </div>
           </div>
@@ -30,10 +50,26 @@ export default function Home() {
           {/* header footer */}
           <div className="md:flex md:mt-16 mt-12 flex-row-reverse">
             {/* Social Media */}
-            <div data-wow-duration="2s" data-wow-delay="2s" className="md:flex md:w-1/2 justify-around px-6 md:px-0 wow fadeInRight">
-              <Button title="@rezak400" image="github.png" target="https://github.com/rezak400" />
-              <Button title="@reza.dwip" image="instagram.png" target="https://www.instagram.com/reza.dwip/" />
-              <Button title="@rezak400" image="twitter.png" target="https://twitter.com/rezak400" />
+            <div
+              data-wow-duration="2s"
+              data-wow-delay="2s"
+              className="md:flex md:w-1/2 justify-around px-6 md:px-0 wow fadeInRight"
+            >
+              <Button
+                title="@rezak400"
+                image="github.png"
+                target="https://github.com/rezak400"
+              />
+              <Button
+                title="@reza.dwip"
+                image="instagram.png"
+                target="https://www.instagram.com/reza.dwip/"
+              />
+              <Button
+                title="@rezak400"
+                image="twitter.png"
+                target="https://twitter.com/rezak400"
+              />
             </div>
 
             {/* Project Link */}
@@ -46,9 +82,8 @@ export default function Home() {
               </Link>
             </div>
           </div>
-
         </div>
       </div>
     </Layout>
-  )
+  );
 }
